a9f361fa5cfdb26ad49671b5ee31980b7c90cf6c
Jacob Nelson <mail@jacobnelson.tech>
2020-08-27 22:20:24 -0500

    1480-running-sum-1d-array

---
+++ 14/1480-running-sum-1d-array/index.js
@@ -0,0 +1,41 @@
+// difficulty: easy
+// link: https://leetcode.com/problems/running-sum-of-1d-array/
+/*
+  Example 1:
+  ------------------------
+  Input: nums = [1,2,3,4]
+  Output: [1,3,6,10]
+  Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+   Example 2:
+  ------------------------
+  Input: nums = [1,1,1,1,1]
+  Output: [1,2,3,4,5]
+  Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+
+  Example 3:
+  ------------------------
+  Input: nums = [3,1,2,10,1]
+  Output: [3,4,6,16,17]
+
+
+  Constraints:
+  ------------------------
+    *  1 <= nums.length <= 1000
+    * -10^6 <= nums[i] <= 10^6
+
+*/
+function runningSum(nums) {
+    if (nums.length === 0)
+        return [];
+    if (nums.length === 1)
+        return nums;
+    var results = [nums[0]];
+    for (var i = 1; i < nums.length; i++) {
+        results.push(results[i - 1] + nums[i]);
+    }
+    return results;
+}
+console.log(runningSum([1, 2, 3, 4]));
+console.log(runningSum([1, 1, 1, 1, 1]));
+console.log(runningSum([3, 1, 2, 10, 1]));
