d1494883da6a81bed657dbe62099bd58b574c918
Jacob Nelson <jacob.nelson@jovio.com>
2020-08-28 00:16:26 -0500

    1431-kids-with-the-greatest-number-of-candies

---
+++ 14/1431-kids-with-most-candies/compiled.js
@@ -0,0 +1,27 @@
+function kidsWithCandies(candies, extraCandies) {
+    var max = candies[0];
+    for (var i = 1; i < candies.length; i++) {
+        if (candies[i] > max)
+            max = candies[i];
+    }
+    var results = [];
+    for (var i = 0; i < candies.length; i++) {
+        if (candies[i] + extraCandies >= max)
+            results.push(true);
+        else
+            results.push(false);
+    }
+    return results;
+}
+console.time('attempt 1');
+console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+console.timeEnd('attempt 1');
+console.log();
+console.time('attempt 2');
+console.log(kidsWithCandies([4, 2, 1, 1, 2], 1));
+console.timeEnd('attempt 2');
+console.log();
+console.time('attempt 3');
+console.log(kidsWithCandies([12, 1, 12], 10));
+console.timeEnd('attempt 3');
+console.log();
