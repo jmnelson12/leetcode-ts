ca393187a7b9210ccb9348d06daa12242f9c44a3
Jacob Nelson <jacob.nelson@jovio.com>
2020-08-29 00:04:35 -0500

    0771-jewels-and-stones

---
+++ 07/0771-jewels-and-stones/index.ts
@@ -0,0 +1,45 @@
+// difficulty: easy
+// link: https://leetcode.com/problems/jewels-and-stones/
+
+/*
+  Example 1:
+  ------------------------
+  Input: J = "aA", S = "aAAbbbb"
+  Output: 3
+
+  Example 2:
+  ------------------------
+  Input: J = "z", S = "ZZ"
+  Output: 0
+
+
+  Constraints:
+  ------------------------
+    * S and J will consist of letters and have length at most 50.
+    * The characters in J are distinct.
+
+*/
+
+function numJewelsInStones(J: string, S: string): number {
+  const jewels = J.split('')
+  let total = 0
+
+  let i = S.length
+  while (i--) {
+    if (jewels.includes(S.charAt(i))) {
+      total++
+    }
+  }
+
+  return total
+}
+
+console.time('attempt 1')
+console.log(numJewelsInStones('aA', 'aAAbbbb'))
+console.timeEnd('attempt 1')
+console.log()
+
+console.time('attempt 2')
+console.log(numJewelsInStones('z', 'ZZ'))
+console.timeEnd('attempt 2')
+console.log()
