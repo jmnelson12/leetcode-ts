316cca5182f3ddc8ff9ab75e6294b3c6d0c019ef
Jacob Nelson <mail@jacobnelson.tech>
2020-10-11 16:20:33 -0500

    second

---
+++ algorithm/quick-sort/second/compiled.js
@@ -0,0 +1,60 @@
+function generateArrOfRandomNumbers(size, _a) {
+    var _b = _a === void 0 ? {} : _a, _c = _b.min, min = _c === void 0 ? 1 : _c, _d = _b.max, max = _d === void 0 ? 5000 : _d;
+    var arr = new Array(size - 1);
+    for (var i = 0; i < size; i++) {
+        arr[i] = Math.floor(Math.random() * (max - min) + min);
+    }
+    return arr;
+}
+function swap(arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+function partition(arr, start, end) {
+    var pivotValue = arr[end];
+    var swapIndex = start;
+    for (var i = start; i < end; i++) {
+        if (arr[i] < pivotValue) {
+            swap(arr, i, swapIndex);
+            swapIndex++;
+        }
+    }
+    swap(arr, swapIndex, end);
+    return swapIndex;
+}
+function quickSort(arr, start, end) {
+    if (start > end)
+        return;
+    var index = partition(arr, start, end);
+    quickSort(arr, start, index - 1);
+    quickSort(arr, index + 1, end);
+    return arr;
+}
+var small = generateArrOfRandomNumbers(10);
+var medium = generateArrOfRandomNumbers(75);
+var large = generateArrOfRandomNumbers(1000);
+var large2 = generateArrOfRandomNumbers(1000);
+console.log("small arr: [" + small + "]\n");
+console.log("medium arr: [" + medium + "]\n");
+console.log("large arr: [" + large + "]\n");
+console.log('===========================\n===========================\n');
+console.time('runtime');
+quickSort(small, 0, small.length - 1);
+console.log("small sorted: [" + small + "]");
+console.timeEnd('runtime');
+console.log();
+console.time('runtime');
+quickSort(medium, 0, medium.length - 1);
+console.log("medium sorted: [" + medium + "]");
+console.timeEnd('runtime');
+console.log();
+console.time('runtime');
+quickSort(large, 0, large.length - 1);
+console.log("large sorted: [" + large + "]");
+console.timeEnd('runtime');
+console.log();
+console.time('runtime');
+console.log("large sorted: [" + large2.sort(function (a, b) { return a - b; }) + "]");
+console.timeEnd('runtime');
+console.log();
---
+++ algorithm/quick-sort/second/index.ts
@@ -0,0 +1,88 @@
+/*
+  Space Complexity:
+
+  Time Complexity:
+    Avg:
+    Worst:
+
+*/
+
+// utils
+function generateArrOfRandomNumbers(
+  size: number,
+  { min = 1, max = 5000 }: { min?: number; max?: number } = {}
+) {
+  const arr: number[] = new Array(size - 1)
+
+  for (let i = 0; i < size; i++) {
+    arr[i] = Math.floor(Math.random() * (max - min) + min)
+  }
+
+  return arr
+}
+
+function swap(arr: number[], i: number, j: number) {
+  let temp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = temp
+}
+
+// Implementation
+function partition(arr: number[], start: number, end: number): number {
+  const pivotValue = arr[end]
+  let swapIndex = start
+
+  for (let i = start; i < end; i++) {
+    if (arr[i] < pivotValue) {
+      swap(arr, i, swapIndex)
+      swapIndex++
+    }
+  }
+
+  swap(arr, swapIndex, end)
+  return swapIndex
+}
+function quickSort(arr: number[], start: number, end: number) {
+  if (start > end) return
+
+  const index = partition(arr, start, end)
+  quickSort(arr, start, index - 1)
+  quickSort(arr, index + 1, end)
+
+  return arr
+}
+
+// Testing
+const small = generateArrOfRandomNumbers(10)
+const medium = generateArrOfRandomNumbers(75)
+const large = generateArrOfRandomNumbers(1000)
+const large2 = generateArrOfRandomNumbers(1000)
+
+console.log(`small arr: [${small}]\n`)
+console.log(`medium arr: [${medium}]\n`)
+console.log(`large arr: [${large}]\n`)
+
+console.log('===========================\n===========================\n')
+
+console.time('runtime')
+quickSort(small, 0, small.length - 1)
+console.log(`small sorted: [${small}]`)
+console.timeEnd('runtime')
+console.log()
+
+console.time('runtime')
+quickSort(medium, 0, medium.length - 1)
+console.log(`medium sorted: [${medium}]`)
+console.timeEnd('runtime')
+console.log()
+
+console.time('runtime')
+quickSort(large, 0, large.length - 1)
+console.log(`large sorted: [${large}]`)
+console.timeEnd('runtime')
+console.log()
+
+console.time('runtime')
+console.log(`large sorted: [${large2.sort((a, b) => a - b)}]`)
+console.timeEnd('runtime')
+console.log()
